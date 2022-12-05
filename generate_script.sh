fcb0050fd91e0a1ce8edf58cf27a919efe49d0fe
DennisBohm1 <d.bohm@student.tudelft.nl>
2022-12-05 10:37:34 +0100

    Add run script

---
+++ generate_script.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#SBATCH --job-name=generate_ct_scan
+#SBATCH --partition=compute
+#SBATCH --account=Education-EEMCS-MSc-CS
+#SBATCH --time=00:30:00
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2GB
+
+module load 2022r2
+module load openmpi
+module load python
+module load py-matplotlib
+module load py-numpy
+module load py-scikit-image
+module load py-pydicom
+module load py-gdcm
+module load py-pylibjpeg-libjpeg
+srun python graf-main/render_xray_G_Z.py > output.log
